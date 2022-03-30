156bf30e01b14d6a523c499921ed2b4f8aac01ed
little fairy <30365474+2544632b@users.noreply.github.com>
2022-03-30 16:14:09 +0800

    Update status.cpp

--- SimpleHttpServer/status.cpp
+++ SimpleHttpServer/status.cpp
@@ -4,40 +4,46 @@
 using namespace std;
 
 string status::getStatus(string status_code) {
-	if (strcmp(status_code.c_str(), "server_ok") == 0) {
-		return "HTTP/1.1 200\n";
-	}
-	else if (strcmp(status_code.c_str(), "not_found") == 0) {
-		return "HTTP/1.1 400\n";
-	}
-	else if (strcmp(status_code.c_str(), "forbidden") == 0) {
-		return "HTTP/1.1 403\n";
-	}
-	return NULL;
+    if(strcmp(status_code.c_str(), "server_ok") == 0) {
+        return "HTTP/1.1 200\n";
+    }
+    else if(strcmp(status_code.c_str(), "not_found") == 0) {
+        return "HTTP/1.1 404\n";
+    }
+    else if(strcmp(status_code.c_str(), "forbidden") == 0) {
+        return "HTTP/1.1 403\n";
+    }
+    return NULL;
 }
 
 string status::getServer(string server_type) {
-	return "Server: " + server_type + "\n";
+    return "Server: "+server_type+"\n";
 }
 
 string status::getDate() {
-	time_t serverDate = time(0);
-	tm* getDate = gmtime(&serverDate);
-	char* getDateChar = asctime(getDate);
-	return "Date: " + string(getDateChar);
+    time_t serverDate = time(0);
+    tm* getDate = gmtime(&serverDate);
+    char* getDateChar = asctime(getDate);
+    return "Date: "+string(getDateChar);
 }
 
 string status::getFileType(string file_type) {
-	if (strcmp(file_type.c_str(), "html") == 0) {
-		return "Content-Type: text/html\n";
-	}
-	if (strcmp(file_type.c_str(), "css") == 0) {
-		return "Content-Type: text/css\n";
-	}
-	if (strcmp(file_type.c_str(), "javascript") || strcmp(file_type.c_str(), "js")) {
-		return "Content-Type: x-application/javascript";
-	}
-	return NULL;
+    if(strcmp(file_type.c_str(), "html") == 0) {
+        return "Content-Type: text/html\n";
+    }
+    else if (strcmp(file_type.c_str(), "css") == 0) {
+        return "Content-Type: text/css\n";
+    }
+    else if (strcmp(file_type.c_str(), "javascript") || strcmp(file_type.c_str(), "js") == 0) {
+        return "Content-Type: x-application/javascript\n";
+    }
+    else {
+        return "Content-Type: text/binary\n";
+    }
+}
+
+string status::getHttpLength(long length) {
+    return "Content-Length: " + to_string(length) + "\n";
 }
 
 string status::getHeader(string status_code,
