cec3afb6ec863a62a1c3fd28e461acf262388c4a
little fairy <30365474+2544632b@users.noreply.github.com>
2022-03-30 16:13:50 +0800

    Update status.h

--- SimpleHttpServer/status.h
+++ SimpleHttpServer/status.h
@@ -1,13 +1,12 @@
 #include <iostream>
-#pragma warning(disable : 4996)
 using namespace std;
 
 class status {
 public:
-	static string getStatus(string status_code);
-	static string getServer(string server_type);
-	static string getDate();
-	static string getFileType(string file_type);
-	static string getHeader(string status_code, string server_type, string file_type, long length);
+    static string getStatus(string status_code);
+    static string getServer(string server_type);
+    static string getDate();
+    static string getFileType(string file_type);
+    static string getHttpLength(long length);
+    static string getHeader(string status_code, string server_type, string file_type, long length);
 };
-// Access control next
